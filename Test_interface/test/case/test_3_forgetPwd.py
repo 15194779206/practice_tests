bdca984ae6616e32d81b0df44f7fb8dd6eb21bc1
ll <2438401528@qq.com>
2021-01-15 18:32:29 +0800

    test

--- Test_interface/test/case/test_3_forgetPwd.py
+++ Test_interface/test/case/test_3_forgetPwd.py
@@ -9,16 +9,10 @@ datas ={
     'nationcode_id': '214',
     'nationcode': '86',
 }
-# data ={
-# "email":"15194779206",
-# "verify_code": "414628",
-# "password": "MTIzNDU2",
-# "nationcode_id": "214",
-# "nationcode": "86"
-# }
-cookies ={
-    'resetpwd_email_check':'e5275c8b81e814a36fc16af294141040'
-}
 
-res= requests.post(url=forgetPwd_ulr, data=datas, headers=headers_choose(), cookies=cookies)
+# cookies ={
+#     'resetpwd_email_check':'e5275c8b81e814a36fc16af294141040'
+# }
+
+res= requests.post(url=forgetPwd_ulr, data=datas, headers=headers_choose())
 print(res.json())
