85c766b2ccd4e8268804cb43070c4a7cdf706a7a
lyang <2438401528@qqq.com>
2021-09-12 18:27:24 +0800

    testing

---
+++ Java/src/Base_java1/Day02/Base1.java
@@ -0,0 +1,21 @@
+package Base_java1.Day02;
+
+public class Base1 {
+   public static void main(String args[]){
+//        System.out.print("不需要换行");
+//        System.out.println("需要换行");
+       /*变量的声明*/
+       /*int x;
+       x=30;
+       x=x-10;
+       System.out.println(x);*/
+       /*定义两个变量*/
+      /* int x=20,y=8;
+       x=x-y;
+       System.out.println(x);*/
+      /*驼峰命名法*/
+//      int lileiAcore,hanTal,lilyAge,carMoney,ShoopNum
+      char a='w';
+      System.out.println(a);
+   }
+}
---
+++ Java/src/Base_java1/Day02/Base2.java
@@ -0,0 +1,14 @@
+package Base_java1.Day02;
+
+public class Base2 {
+    public static void main(String args[]){
+//隐式类型转换
+        int x=11;
+        double vge=x/3;
+        System.out.println(vge);
+// 强制类型转换
+        double p=187.55;
+        int f=(int)(p/100);
+        System.out.println(f);  //1
+    }
+}
